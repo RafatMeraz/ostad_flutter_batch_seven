7961b67920a97a9fdb22ed489964af9c1b83599c
RafatMeraz <razhossain201@gmail.com>
2024-05-21 22:16:15 +0600

    Initial commit

---
+++ basic_dart/hello_world.dart
@@ -0,0 +1,17 @@
+main() {
+  // single line comment
+  print('Good morning');
+  print('Hello world, we are writing dart code');
+  print(93849839489);
+  print(3.1416);
+  /*
+  Multiline comment
+  Multiline comment
+  Multiline comment
+  Multiline comment
+   */
+  print(12+45);
+  print(4 * 45);
+  print(4 / 45);
+  print(4 - 45);
+}
---
+++ basic_dart/variables.dart
@@ -0,0 +1,49 @@
+main() {
+  // Primitive data types
+  // String(textual data), integer(int), double(floating value), boolean(bool)
+
+  /* print('usern\'ame');
+  print("user\"name");
+  print('''username ' "
+  
+  sdsdsd
+  ssadsa ''');
+
+  print(3423437848);
+  print(233434.43434343);
+  print(true);
+  print(true);
+
+  */
+
+  // variable -> container -> contains a value
+  // data-type variable-name = value;
+  String userName = 'Rafat Meraz';
+  print(userName);
+  userName = 'Meraz';
+  print(userName);
+  userName = 'safsdf';
+  print(userName);
+
+  int age = 30;
+  print(age);
+  age = 35;
+  print(age);
+
+  double cgpa = 3.56;
+  print(cgpa);
+  cgpa = 3.513489984;
+  print(cgpa);
+
+  bool isSuccess = false;
+  print(isSuccess);
+  isSuccess = true;
+  print(isSuccess);
+
+  // camelCase *******
+  // myName
+  // snack_case
+  // Pascal
+  // kebab-case
+  // SCREAMING
+}
