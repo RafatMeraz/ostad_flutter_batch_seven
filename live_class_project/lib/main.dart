583dfc5a8366c8d0d10ad0daae924d0036850799
RafatMeraz <razhossain201@gmail.com>
2024-07-12 23:19:41 +0600

    Container, dialog, bottom-sheet

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,7 +1,7 @@
 // Current SDK version: 3.22.1
 import 'package:flutter/material.dart';
 
-// ListView, Gridview, SizedBox, ListTile, Divider
+// Container, dialog, bottom-sheet
 
 void main() {
   runApp(IntroApp());
@@ -38,222 +38,145 @@ class _HomeState extends State<Home> {
         title: Text('Home'),
         backgroundColor: Colors.blue,
       ),
-      // body: ListView(
-      //   children: [
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 1'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 2'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 3'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 1'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 2'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 3'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 1'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 2'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 3'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 1'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 2'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 3'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 1'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 2'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 3'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 1'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 2'),
-      //       ),
-      //     ),
-      //     SizedBox(
-      //       height: 100,
-      //       width: 100,
-      //       child: Center(
-      //         child: Text('Box 3'),
-      //       ),
-      //     ),
-      //   ],
-      // ),
-      // body: ListView.builder(
-      //   itemCount: 1000, // 0-999
-      //   itemBuilder: (context, index) {
-      //     return SizedBox(
-      //       width: 100,
-      //       height: 100,
-      //       child: Center(child: Text(index.toString())),
-      //     );
-      //   },
-      // ),
-      // body: ListView.builder(
-      //   itemCount: friendList.length,
-      //   // scrollDirection: Axis.horizontal,
-      //   itemBuilder: (context, index) {
-      //     return Text(friendList[index]);
-      //   },
-      // ),
-      body: ListView.separated(
-        itemCount: friendList.length,
-        // scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return ListTile(
-            tileColor: Colors.grey.shade50,
-            title: Text(friendList[index]),
-            subtitle: Text('School friend'),
-            trailing: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.do_not_disturb_on_total_silence, color: Colors.green, size: 10,),
-                Text('Active')
-              ],
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              // color: Colors.green,
+              // padding: EdgeInsets.all(8),
+              // padding: EdgeInsets.symmetric(
+              //   vertical: 8,
+              //   horizontal: 4
+              // ),
+              padding: EdgeInsets.only(bottom: 8),
+              margin: EdgeInsets.all(16),
+              alignment: Alignment.bottomRight,
+              decoration: BoxDecoration(
+                  color: Colors.green,
+                  border: Border.all(color: Colors.red, width: 4),
+                  // borderRadius: BorderRadius.circular(16)
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16)),
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/messi.jpg'),
+                      fit: BoxFit.cover,
+                      opacity: 0.4),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(
+                      color: Colors.red.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 8,
+                      offset: Offset(0, 3),
+                    ),
+                    BoxShadow(
+                      color: Colors.green.withOpacity(0.2),
+                      spreadRadius: 3,
+                      blurRadius: 8,
+                      offset: Offset(0, 6),
+                    ),
+                  ]),
+              child: Text('Messi'),
             ),
-            leading: Text((index+1).toString()),
-            onTap: () {
-              print('$index item tapped');
-            },
-            titleTextStyle: TextStyle(
-              fontSize: 18,
-              color: Colors.red
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.green,
+              padding: EdgeInsets.all(16),
+              margin: EdgeInsets.all(8),
             ),
-          );
-        },
-        separatorBuilder: (context, index) {
-          return Divider(
-            color: Colors.grey.shade200,
-            thickness: 2,
-            endIndent: 16,
-            indent: 4,
-          );
-          // return Text('This is $index separator');
-        },
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text('sdfsdf'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // showAboutDialog(
+                //   context: context,
+                //   applicationName: 'Sample',
+                //   applicationVersion: '1.0.1',
+                //   children: [
+                //     Text('Sample text')
+                //   ]
+                // );
+                showDialog(
+                    context: context,
+                    barrierDismissible: false,
+                    barrierColor: Colors.black54,
+                    builder: (ctx) {
+                      return AlertDialog(
+                        title: Text('Our custom dialog'),
+                        content: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Sample text'),
+                            Text('Sample text'),
+                            Text('Sample text'),
+                          ],
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text('Okay'),
+                          )
+                        ],
+                        shape: RoundedRectangleBorder(
+                            // borderRadius: BorderRadius.circular(0)
+                            borderRadius: BorderRadius.circular(8)),
+                        backgroundColor: Colors.white,
+                      );
+                    });
+              },
+              child: Text('Dialog'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  backgroundColor: Colors.white,
+                  context: context,
+                  barrierColor: Colors.black54,
+                  enableDrag: false,
+                  isDismissible: false,
+                  builder: (ctx) {
+                    return SizedBox(
+                      height: 600,
+                      width: double.infinity,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text('Title'),
+                          ),
+                          Divider(),
+                          Column(
+                            children: [
+
+                            ],
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Text('Show Bottom sheet'),
+            )
+          ],
+        ),
       ),
-      // body: GridView.builder(
-      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //     crossAxisCount: 3,
-      //     crossAxisSpacing: 4,
-      //     mainAxisSpacing: 10,
-      //     childAspectRatio: 1
-      //   ),
-      //   itemCount: friendList.length,
-      //   itemBuilder: (context, index) {
-      //     return Column(
-      //       children: [
-      //         Text(index.toString()),
-      //         Text(friendList[index]),
-      //       ],
-      //     );
-      //   },
-      // ),
-      // body: GridView.builder(
-      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //     crossAxisCount: 3,
-      //     crossAxisSpacing: 4,
-      //     mainAxisSpacing: 10,
-      //     childAspectRatio: 1
-      //   ),
-      //   itemCount: friendList.length,
-      //   itemBuilder: (context, index) {
-      //     return Column(
-      //       children: [
-      //         Text(index.toString()),
-      //         Text(friendList[index]),
-      //       ],
-      //     );
-      //   },
-      // ),
     );
   }
 }
