e490949dca3d671bfbb26c05b69aff332933af78
RafatMeraz <razhossain201@gmail.com>
2024-07-14 23:24:23 +0600

    Add TextField, Theme

--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,7 +1,7 @@
 // Current SDK version: 3.22.1
 import 'package:flutter/material.dart';
 
-// Container, dialog, bottom-sheet
+// TextField
 
 void main() {
   runApp(IntroApp());
@@ -16,6 +16,107 @@ class IntroApp extends StatelessWidget {
       debugShowCheckedModeBanner: true,
       home: Home(),
       title: 'Intro app',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.pink,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.w700,
+            letterSpacing: 1.2
+          )
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.pink,
+            textStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w700
+            )
+          )
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(
+                  color: Colors.green,
+                  width: 2
+              )
+          ),
+          disabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(
+                  color: Colors.green,
+                  width: 2
+              )
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+              borderSide: BorderSide(
+                  color: Colors.blueAccent,
+                  width: 2
+              )
+          ),
+          labelStyle: TextStyle(
+              fontSize: 16
+          ),
+          hintStyle: TextStyle(
+              color: Colors.purple.shade200
+          ),
+        )
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.green,
+              titleTextStyle: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 1.2
+              )
+          ),
+          textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+                  foregroundColor: Colors.green,
+                  textStyle: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700
+                  )
+              )
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                    color: Colors.green,
+                    width: 2
+                )
+            ),
+            disabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                    color: Colors.green,
+                    width: 2
+                )
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                    color: Colors.blueAccent,
+                    width: 2
+                )
+            ),
+            labelStyle: TextStyle(
+                fontSize: 16
+            ),
+            hintStyle: TextStyle(
+                color: Colors.purple.shade200
+            ),
+          )
+      ),
+      themeMode: ThemeMode.system,
     );
   }
 }
@@ -28,155 +129,121 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<String> friendList = ['Fahad', 'Iram', 'Touhid', 'Sakib', 'Niloy'];
+  TextEditingController _descriptionTEController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        // backgroundColor: Colors.blue,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                enabled: true,
+                decoration: InputDecoration(
+                  label: Text('Name'),
+                  labelStyle: TextStyle(
+                    fontSize: 16
+                  ),
+                  hintText: 'Enter your name',
+                  hintStyle: TextStyle(
+                    color: Colors.green.shade200
+                  ),
+                  icon: Icon(Icons.add),
+                  prefixIcon: Icon(Icons.search),
+                  suffixIcon: TextButton(
+                    child: Text('Search'),
+                    onPressed: () {},
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(
+                          color: Colors.green,
+                          width: 2
+                      )
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                      width: 2
+                    )
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide(
+                          color: Colors.blueAccent,
+                          width: 2
+                      )
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _descriptionTEController,
+                enabled: true,
+                maxLines: 5,
+                maxLength: 200,
+                onTap: () {
+                  print('Tapped on textfield');
+                },
+                onChanged: (String value) {
+                  print(value);
+                },
+                decoration: InputDecoration(
+                  label: Text('Description'),
+                  labelStyle: TextStyle(
+                    fontSize: 16
+                  ),
+                  hintText: 'Enter your description',
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                enabled: true,
+                onChanged: (String value) {
+                  print(value);
+                },
+                obscureText: true,
+                controller: TextEditingController(),
+                decoration: InputDecoration(
+                  label: Text('Password'),
+                  labelStyle: TextStyle(
+                    fontSize: 16
+                  ),
+                  hintText: 'Enter your password',
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(),
+              TextButton(onPressed: () {
+                // _descriptionTEController.text = 'clear';
+                _descriptionTEController.clear();
+              }, child: Text('Clear description'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Home'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 100,
-              height: 100,
-              // color: Colors.green,
-              // padding: EdgeInsets.all(8),
-              // padding: EdgeInsets.symmetric(
-              //   vertical: 8,
-              //   horizontal: 4
-              // ),
-              padding: EdgeInsets.only(bottom: 8),
-              margin: EdgeInsets.all(16),
-              alignment: Alignment.bottomRight,
-              decoration: BoxDecoration(
-                  color: Colors.green,
-                  border: Border.all(color: Colors.red, width: 4),
-                  // borderRadius: BorderRadius.circular(16)
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      bottomRight: Radius.circular(16)),
-                  shape: BoxShape.rectangle,
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/messi.jpg'),
-                      fit: BoxFit.cover,
-                      opacity: 0.4),
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.red.withOpacity(0.5),
-                      spreadRadius: 3,
-                      blurRadius: 8,
-                      offset: Offset(0, 3),
-                    ),
-                    BoxShadow(
-                      color: Colors.green.withOpacity(0.2),
-                      spreadRadius: 3,
-                      blurRadius: 8,
-                      offset: Offset(0, 6),
-                    ),
-                  ]),
-              child: Text('Messi'),
-            ),
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.green,
-              padding: EdgeInsets.all(16),
-              margin: EdgeInsets.all(8),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(24.0),
-              child: Text('sdfsdf'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                // showAboutDialog(
-                //   context: context,
-                //   applicationName: 'Sample',
-                //   applicationVersion: '1.0.1',
-                //   children: [
-                //     Text('Sample text')
-                //   ]
-                // );
-                showDialog(
-                    context: context,
-                    barrierDismissible: false,
-                    barrierColor: Colors.black54,
-                    builder: (ctx) {
-                      return AlertDialog(
-                        title: Text('Our custom dialog'),
-                        content: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Sample text'),
-                            Text('Sample text'),
-                            Text('Sample text'),
-                          ],
-                        ),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text('Cancel'),
-                          ),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text('Okay'),
-                          )
-                        ],
-                        shape: RoundedRectangleBorder(
-                            // borderRadius: BorderRadius.circular(0)
-                            borderRadius: BorderRadius.circular(8)),
-                        backgroundColor: Colors.white,
-                      );
-                    });
-              },
-              child: Text('Dialog'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                showModalBottomSheet(
-                  backgroundColor: Colors.white,
-                  context: context,
-                  barrierColor: Colors.black54,
-                  enableDrag: false,
-                  isDismissible: false,
-                  builder: (ctx) {
-                    return SizedBox(
-                      height: 600,
-                      width: double.infinity,
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Text('Title'),
-                          ),
-                          Divider(),
-                          Column(
-                            children: [
-
-                            ],
-                          )
-                        ],
-                      ),
-                    );
-                  },
-                );
-              },
-              child: Text('Show Bottom sheet'),
-            )
-          ],
-        ),
+        title: Text('Settings'),
       ),
     );
   }
 }
+
