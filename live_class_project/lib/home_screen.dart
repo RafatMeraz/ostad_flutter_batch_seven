c904abae31aadb1142f0d751c785a2e04b3caee4
RafatMeraz <razhossain201@gmail.com>
2024-11-05 23:26:47 +0600

    Add getx navigation

--- live_class_project/lib/home_screen.dart
+++ live_class_project/lib/home_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:live_class_project/counter_controller.dart';
+import 'package:live_class_project/settings_screen.dart';
 
 // Obs, Obx, GetxController, GetBuilder,
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  static const String name = '/';
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -19,16 +22,56 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Home'),
       ),
       body: Center(
-        child: GetBuilder<CounterController>(
-          builder: (counterController) {
-            return Text(
-              '${counterController.counter}',
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w600,
-              ),
-            );
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GetBuilder<CounterController>(
+              builder: (counterController) {
+                return Text(
+                  '${counterController.counter}',
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => const SettingsScreen(),
+                //   ),
+                // );
+                // Navigator.pushNamed(context, '/settings');
+                // Navigator.pushReplacementNamed(context, '/settings');
+                // Navigator.pushNamedAndRemoveUntil(
+                //   context,
+                //   '/settings',
+                //   (predicate) => false,
+                // );
+                // Navigator.pushNamed(context, SettingsScreen.name);
+                // Get.to(() => const SettingsScreen());
+                Get.toNamed(SettingsScreen.name);
+                // Get.offNamed(page) // Replacement
+              },
+              child: const Text('Settings'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Get.showSnackbar(
+                  const GetSnackBar(
+                    title: 'Title',
+                    message: 'This is message',
+                    duration: Duration(seconds: 3),
+                    snackPosition: SnackPosition.TOP,
+                  ),
+                );
+              },
+              child: const Text('Show Snack Bar'),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:live_class_project/counter_controller.dart';
 import 'package:live_class_project/home_screen.dart';
+import 'package:live_class_project/profile_screen.dart';
+import 'package:live_class_project/settings_screen.dart';
 
 void main() {
   runApp(const CounterApp());
@@ -13,8 +15,13 @@ class CounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: const HomeScreen(),
       initialBinding: ControllerBinder(),
+      initialRoute: '/',
+      routes: {
+        HomeScreen.name: (context) => const HomeScreen(),
+        ProfileScreen.name: (context) => const ProfileScreen(),
+        SettingsScreen.name: (context) => const SettingsScreen(),
+      },
     );
   }
 }
