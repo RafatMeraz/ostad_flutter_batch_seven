5e339dff0927d1fc12d6e825598190d5e01966ac
RafatMeraz <razhossain201@gmail.com>
2024-11-19 22:25:27 +0600

    Add firebase set up for android

--- live_class_project/android/app/src/main/kotlin/com/ostad/live_class_project/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ostad.live_class_project
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:live_class_project/counter_controller.dart';
+import 'package:live_class_project/firebase_options.dart';
 import 'package:live_class_project/home_screen.dart';
 import 'package:live_class_project/profile_screen.dart';
 import 'package:live_class_project/settings_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const CounterApp());
 }
 
