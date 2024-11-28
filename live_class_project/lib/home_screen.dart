6e9959e6f2325946d6d3041057e42fa218aa44a5
RafatMeraz <razhossain201@gmail.com>
2024-11-28 23:30:52 +0600

    Add basic google maps

---
+++ live_class_project/lib/home_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late GoogleMapController googleMapController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map Screen'),
+      ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: const CameraPosition(
+          zoom: 16,
+          target: LatLng(
+            23.800507141895935,
+            90.3722262378507,
+          ),
+        ),
+        onTap: (LatLng? latLng) {
+          print(latLng);
+        },
+        zoomControlsEnabled: true,
+        zoomGesturesEnabled: true,
+        onMapCreated: (GoogleMapController controller) {
+          googleMapController = controller;
+        },
+        trafficEnabled: true,
+      ),
+    );
+  }
+}
--- live_class_project/lib/main.dart
+++ live_class_project/lib/main.dart
@@ -1,28 +1,18 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:live_class_project/firebase_notification_manager.dart';
-import 'package:live_class_project/live_score_screen.dart';
+import 'package:live_class_project/home_screen.dart';
 
-import 'firebase_options.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  await FirebaseNotificationManager.initialize();
-  print(await FirebaseNotificationManager.getFcmToken());
-  runApp(const LiveScoreApp());
+void main() {
+  runApp(const GoogleMapsApp());
 }
 
-class LiveScoreApp extends StatelessWidget {
-  const LiveScoreApp({super.key});
+
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: LiveScoreScreen(),
+    return const MaterialApp(
+      home: HomeScreen(),
     );
   }
 }
